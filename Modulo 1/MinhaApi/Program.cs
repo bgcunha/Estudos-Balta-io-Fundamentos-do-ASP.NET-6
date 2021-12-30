17baaaa0e14c4272ec35838d4a5138cd69f755d8
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-30 11:11:24 -0300

    Demostração simples minimal API

--- Modulo 1/MinhaApi/Program.cs
+++ Modulo 1/MinhaApi/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return Results.Ok("Hello World");
+});
+
+app.MapGet("/{nome}", (string nome) =>
+{
+    return Results.Ok($"Hello {nome}");
+});
+
+app.MapGet("/name/{nome}", (string nome) =>
+{
+    return Results.Ok($"Hello {nome}");
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok($"ID: {user.Id} e o Nome: {user.Username}");
+});
 
 app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+}
