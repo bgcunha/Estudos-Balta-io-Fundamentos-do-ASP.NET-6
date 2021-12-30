9a9a0d800a728e6e97d30f4098fb63bb3a18be58
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-30 14:54:44 -0300

    CRUD TDO

--- Modulo 2/Todo/Controllers/HomeController.cs
+++ Modulo 2/Todo/Controllers/HomeController.cs
@@ -17,6 +17,51 @@ public class HomeControoller : ControllerBase
         return context.Todos.ToList();
     }
 
+    [HttpGet]
+    [Route("/{id:int}")]
+    public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+    {
+        return context.Todos.FirstOrDefault(x => x.Id == id);
+    }
+
+    [HttpPost]
+    [Route("/")]
+    public TodoModel Post([FromBody] TodoModel model, [FromServices] AppDbContext context)
+    {
+        context.Todos.Add(model);
+        context.SaveChanges();
+
+        return model;
+    }
+
+    [HttpPut]
+    [Route("/{id:int}")]
+    public TodoModel Put([FromRoute] int id, [FromBody] TodoModel model, [FromServices] AppDbContext context)
+    {
+        var todo = GetById(id, context);
+        if (todo == null)
+            return model;
+
+        todo.Title = model.Title;
+        todo.Done = model.Done;
+
+        context.Todos.Update(todo);
+        context.SaveChanges();
+
+        return todo;
+    }
+
+    [HttpDelete]
+    [Route("/{id:int}")]
+    public TodoModel Delete([FromRoute] int id, [FromBody] TodoModel model, [FromServices] AppDbContext context)
+    {
+        var todo = GetById(id, context);
+
+        context.Todos.Remove(todo);
+        context.SaveChanges();
+
+        return todo;
+    }
 
 
 }
