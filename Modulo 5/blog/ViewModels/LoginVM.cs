bd6302b5a8807f04d54f36a22a990c3b15d684eb
Bruno Expedito Gomes da Cunha <bruno.gomes.da.cunha@avanade.com>
2022-01-06 15:53:08 -0300

    Estava faltando o LoginVM.cs

---
+++ Modulo 5/blog/ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blog.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatório!")]
+        public string Password { get; set; }
+    }
+}
